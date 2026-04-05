784103a57e9746aedd65388a053f5d239103ecb4
Prakhar Patel <prakhar.codes433@gmail.com>
2026-04-05 10:04:10 +0530

    Add files via upload

---
+++ sentenceMaker/script.js
@@ -0,0 +1,21 @@
+let adjective = "giant";
+let noun = "dragon";
+let verb = "excited";
+let place = "mountain cave";
+let adjective2 = "smoky";
+let noun2 = "tacos";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "tiny";
+noun = "hamster";
+verb = "hungry";
+place = "cardboard castle";
+adjective2 = "glittery";
+noun2 = "blueberries";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
